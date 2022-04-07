a453c42df263f134774cd93cd77a06d6f556afdd
diogo-soares <diogosoaresk8@hotmail.com>
2022-04-06 22:51:44 -0300

    config ip adress

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+
+namespace windows_network_sniffer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sniffer de Rede");
+            Console.WriteLine("+++++++++++++++");
+
+            var ipAdress = SelectIPAddress();
+        }
+
+        static IPAddress SelectIPAddress()
+        {
+            var hostname = Dns.GetHostName();
+            Console.WriteLine("Computador: " + hostname);
+
+            var hostnameEntry = Dns.GetHostEntry(hostname);
+            var ipAddresses = hostnameEntry.AddressList;
+
+            Console.WriteLine("Endereços IP:");
+            for (var i = 0; i < ipAddresses.Length; i++)
+            {
+                var ipAddress = ipAddresses[i];
+                Console.WriteLine("  " + (i + 1) + ") " + ipAddress);
+            }
+
+            int selectedIndex;
+            do
+            {
+                Console.Write("Selecione: ");
+            } while (int.TryParse(Console.ReadLine(), out selectedIndex) == false ||
+                     selectedIndex < 1 ||
+                     selectedIndex > ipAddresses.Length);
+
+            return ipAddresses[selectedIndex - 1];
+        }
+    }
+}
